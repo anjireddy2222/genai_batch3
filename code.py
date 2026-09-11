3007845e4078c6a71f90d6baaea0bfb78270b1cb
Anji Reddy <ardream2222@gmail.com>
2026-09-11 21:00:19 +0530

    s

---
+++ code.py
@@ -0,0 +1,107 @@
+
+print(1233354) # number data
+print("python code##$%^&*") # string data
+print('run my code@gssdh___')
+print("1233354") 
+
+name = "anji reddy"
+
+# a = 10
+# b = 20
+# a + b 
+
+print(  name )
+
+print( "name" )
+print( 'name' )
+
+name = "dd"
+
+print( name )
+
+first_name = "anji"
+last_name = "reddy"
+
+print( first_name + last_name )
+
+print( first_name )
+
+print(last_name )
+
+full_name = first_name + last_name
+
+# a=10
+# b=20
+# c= a+b
+
+print( full_name )
+
+full_name = first_name + " " + last_name
+# anji reddy
+
+print( full_name )
+
+full_name = "Mr. " + first_name + " " + last_name
+
+print( full_name )
+
+# Number data types
+
+product_price = 99
+
+selling_price = 80
+
+discount = product_price - selling_price
+
+print( discount )
+
+selling_price = 99.99
+
+print( selling_price )
+
+selling_price = "joking"
+
+print( selling_price )
+
+
+# boolean data type
+
+is_loggedin = True
+
+Is_video_liked = False
+
+print( is_loggedin )
+
+print( Is_video_liked )
+
+
+# 3 prodcuts
+
+product_one = "iphone"
+product_two = "oneplus"
+product_three = "google pixel"
+
+# list data type or arrays
+
+products = [ "iphone", "oneplus", "google pixel" ]
+
+prices = [ 99, 150, 260 ]
+
+stock_avaliability = [True, False, False]
+
+print( products )
+
+print( prices )
+
+print( stock_avaliability )
+
+
+# dict data type
+
+product_one = {  "price": 99, "is_avaible": True, "title": "iphone" }
+
+print( product_one)
+
+
+
+
