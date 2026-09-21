105111dc9f2149c83dd1acdd87970ab6a0d696b0
Anji Reddy <ardream2222@gmail.com>
2026-09-21 09:54:06 +0530

    s

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@
 # fastapi
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
 
 app = FastAPI()
 
@@ -21,4 +23,28 @@ def login():
 def get_data():
 
     return { "user_id": 1, "token": "asgasgasgasgasg" }
+
+class InputData(BaseModel):
+    user_message: str
+    user_id: int
+
+
+@app.post("/chat")
+def chat( req : InputData ):
+
+    openai_client = openai.OpenAI( api_key = "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" )
+
+    prompt = """
+    you are AI sales assistant for softwareschool. we are providing coding classes in telugu. 
+
+    Course
+    """
+
+    ai_response = openai_client.responses.create( model="gpt-5.5", input=req.user_message )
+
+    ai_response = ai_response.output_text
+
+
+
+    return { "result": "success", "data": ai_response }
 
